8d7c7e39444e407cd9b9f71d152161a1a0840350
Adam <adamwawrzyniak5@gmail.com>
2025-11-08 23:06:49 +0100

    init

---
+++ EspApi/Program.cs
@@ -0,0 +1,30 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+
+
+app.MapPost("/api/sensor", (SensorData data) =>
+{
+    Console.WriteLine($"Otrzymano dane: {data.Temperature}");
+
+    return Results.Ok(new { status = "received", timestamp = DateTime.UtcNow });
+
+});
+app.MapGet("/health", () => Results.Ok(new { status = "OK" }));
+
+app.Run();
+
+record SensorData(float Temperature, string? DeviceId= null);
